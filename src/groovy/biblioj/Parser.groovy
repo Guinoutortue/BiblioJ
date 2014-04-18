fb98c11f4f7be26717026489e77a03d549aec7b9
Guinoutortue <guinou@msn.com>
2014-04-18 09:35:27 +0200

    parsage titres

--- src/groovy/biblioj/Parser.groovy
+++ src/groovy/biblioj/Parser.groovy
@@ -1,7 +1,17 @@
 package biblioj
 
+
 class Parser {
-	public void parserCSV() {
-		
+	public static void parserCSV() {
+		// Insert new codes
+		def csv = new File("grails-app/conf/Les 1000 titres les plus recherches en 2012.csv")
+		def livre
+		csv.splitEachLine(';') { row ->
+		   livre = Livre.findByTitre(row[1]) ?: new Livre(
+			  titre: row[1],
+			  nombreExemplaires: 3,
+			  nombreExemplairesDisponibles: 3
+		   ).save(failOnError: true, flush: true)
+		}
 	}
 }
