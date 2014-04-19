13569498a075f775f845fa7260865676735c0e28
Guinoutortue <guinou@msn.com>
2014-04-19 16:25:33 +0200

    Parsage Auteurs et insertion bd

--- src/groovy/biblioj/Parser.groovy
+++ src/groovy/biblioj/Parser.groovy
@@ -6,7 +6,7 @@ class Parser {
 		def csv = new File("grails-app/conf/Les 1000 titres les plus recherches en 2012.csv")
 		def livre
 		def auteur
-		def model=false;
+		def model=false; // pour ne pas traiter la première ligne
 		csv.splitEachLine(';') { row ->
 			if(model){
 				livre = Livre.findByTitre(row[3]) ?: new Livre(
@@ -16,35 +16,31 @@ class Parser {
 			   ).save(failOnError: true, flush: true)	
 			   
 			   String rowAuteur = row[4]
-			   println "\n"
-			   println rowAuteur
+			   
 			   if(rowAuteur!=null) {
 				   String[] rowNom =  rowAuteur.split("[,]")
-				   println rowNom
-				   boolean nomEtPrenom = rowNom.size()==2
-				   println rowNom.size()
+				   boolean nomEtPrenom = rowNom.size()==2 //pour savoir si il a un nom ET un prenom
 				   
-				   auteur = Auteur.findAllByNom(rowNom[0]).each { a ->
-					   println nomEtPrenom
-					   if(nomEtPrenom) {
-						   a.findByPrenom(rowNom[1]) ?: new Auteur(
+				   if(nomEtPrenom) {
+					    auteur = Auteur.findByNomAndPrenom(rowNom[0],rowNom[1]) ?: new Auteur(
 							   nom: rowNom[0],
-							   prenom: rowNom[1],
-							   Livre.findByTitre(row[3])
-						   	).save(failOnError: true, flush: true)
-					   } else {
-						   new Auteur(
-							   nom: rowNom[0],
-							   prenom: "",
-							   livres: Livre.findByTitre(row[3])
+							   prenom: rowNom[1]
 							   ).save(failOnError: true, flush: true)
-					   }
-					}
-				   println auteur
+					   
+				   } else {
+					   auteur = Auteur.findByNomAndPrenom(rowNom[0],"") ?: new Auteur(
+						   nom: rowNom[0],
+						   prenom: ""
+						   ).save(failOnError: true, flush: true)
+				   }
+				   
+				   //Livre.findByTitre(row[3]).addToAuteurs(Auteur.findByNomAndPrenom(rowNom[0],rowNom[1]))
 			   }
+			   
 			} else {
 				model=true;
 			}
+			
 		}
 	}
 }
