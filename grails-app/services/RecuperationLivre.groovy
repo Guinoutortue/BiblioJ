bb2b14c30fd4a843cc28e74437f9f40750174db3
jordi12 <jordan.blancpoujol@gmail.com>
2014-04-19 16:05:06 +0200

    recuperation livre par nom d'auteur

--- grails-app/services/RecuperationLivre.groovy
+++ grails-app/services/RecuperationLivre.groovy
@@ -1,10 +1,17 @@
 import biblioj.Livre
+import biblioj.Auteur
 
 
 class RecuperationLivre {
 	
 	def getByTitre(String t) {
 		def result = Livre.findAllByTitreLike(t)
+			return result
+
+	}
+	
+	def getByNom(String t) {
+		def result = Auteur.findAllByNomLike(t)
 			return result
 
 	}
