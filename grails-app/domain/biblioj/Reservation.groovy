845b93889f25c75b8e83ee6559b9824c506c9fae
jordi12 <jordan.blancpoujol@gmail.com>
2014-04-21 10:50:31 +0200

    reservation impl

--- grails-app/domain/biblioj/Reservation.groovy
+++ grails-app/domain/biblioj/Reservation.groovy
@@ -9,6 +9,7 @@ class Reservation {
     static constraints = {
 		code validator:{ val -> Reservation.findByCode(val)==null } 
 		code nullable:false 
+		code unique:true
 		dateReservation nullable:false 
 		livres nullable:true
     }
